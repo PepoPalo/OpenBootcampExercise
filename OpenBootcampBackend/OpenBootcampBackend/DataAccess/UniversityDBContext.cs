6e2d77901ad30ae926d516b22e880a0bbb65f538
Pedro Palomino <pedropalominoaraoz@gmail.com>
2023-03-28 00:02:18 -0300

    First commit

---
+++ OpenBootcampBackend/OpenBootcampBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenBootcampBackend.Models.DataModels;
+using System.Security.Policy;
+
+namespace OpenBootcampBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+
+        // TODO: Add DBSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+    }
+}
